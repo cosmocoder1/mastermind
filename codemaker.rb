5780a0af8cb4f74ee5aeed342b79ec61960fe7d0
Nathan Lucy <cosmocoder1@gmail.com>
2021-01-14 11:44:05 -0600

    basic function complete

--- codemaker.rb
+++ codemaker.rb
@@ -8,15 +8,14 @@ module Codemaker
 
   $computer = {
   "code" => [],
-  "previous_guesses" => [],
-  "correct_colors" => [],
-  "correct_place" => 0
+  "previous_guesses" => []
   }
 
   $guess_loop = 0
 
 
   def intro_cm()
+
     puts ""
     puts "0 = red"
     puts "1 = blue"
@@ -30,7 +29,8 @@ module Codemaker
 
     player_code = gets.chomp.split('')
     $player["code"] = player_code.map { |value| value.to_i() }
-    puts $player["code"].inspect()
+    puts $player["code"].map { |num| interpret(num) }.inspect()
+    puts ""
     sleep(2)
 
     puts "Excellent!  The computer will now attempt to guess your code..."
@@ -43,28 +43,26 @@ module Codemaker
   def game_cm_loop()
     
     i = 0 
+
     $computer["code"] = []
 
     #computer guess
-    while i < 4 do
-      random_num = rand(6).to_s
-      if !$computer["code"].include?(random_num) 
-        $computer["code"].push(random_num)
-        i += 1
-      end  
-        $computer["code"] = $computer["code"].map { |value| value.to_i() }
-    end 
-      
-    puts $computer["code"]
-
-    #add computer guess to previous guesses array
-    if !$computer["previous_guesses"].include?($computer["code"])
-      $computer["previous_guesses"].push($computer["code"])  
+    if $computer["previous_guesses"].length() == 0
+      while $computer["code"].length < 4 do
+        random_num = rand(6)
+        unless $computer["code"].include?(random_num) 
+          $computer["code"].push(random_num)
+          i += 1
+        end  
+          $computer["code"] = $computer["code"].map { |value| value.to_i() }
+      end 
     end  
 
+
+    #12 turn game counter
     $guess_loop += 1
 
-    if $guess_loop > 13
+    if $guess_loop > 12
       puts "The computer failed to guess your code!"
       sleep(3)
       system("clear")
@@ -80,50 +78,74 @@ module Codemaker
         game()
       end  
     end  
+
+    #translate numbers to colors
+  def interpret(num) 
+    case num
+      when 0
+        "red"
+      when 1
+        "blue"
+      when 2
+        "green"
+      when 3
+        "yellow"
+      when 4
+        "black"
+      when 5
+        "white" 
+      end
+    end    
     
     
     #player gives hint based on computer's guess
     def give_hint()
       
+      sleep(2)
       puts "hint..."
+      sleep(1)
 
       correct_colors = []
       correct_place = 0
 
+      #count matching colors
       $computer["code"].map { |value| if $player["code"].include?(value)
       correct_colors.push(value) 
       end }
+
       #update object
       $computer["correct_colors"] = correct_colors
 
+      #report matching colors 
       if correct_colors.length() > 0
-      puts "#{correct_colors.length()} of the colors that the computer chose are accurate..."
+        puts "#{correct_colors.length()} of the colors that the computer chose are accurate..."
       end  
-  
+      
+      #count matching placement of matching colors
       correct_colors.map { |value| $computer["code"].index(value) == $player["code"].index(value) }.map { |value| if value == true
       correct_place += 1
       end}
 
+      #update object
       $computer["correct_place"] = correct_place
 
       
       puts "... and #{correct_place} in the correct place" 
       
-      sleep(2)
+      sleep(3)
 
-    end  
+
+    end
       
 
     def report_guess()
       if $computer["code"] != $player["code"]
-        puts "The computer guessed #{$computer["code"].inspect()}"
+        puts "The computer guessed #{$computer["code"].map { |num| interpret(num) }.inspect()}"
         puts ""
         sleep(2)
         give_hint()
         puts ""
         sleep(2)
-        puts "Guessing again..."
-        puts ""
         game_cm_loop()
       end
     end
